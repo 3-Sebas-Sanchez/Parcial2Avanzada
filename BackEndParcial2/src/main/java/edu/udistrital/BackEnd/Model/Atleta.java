ae29a757ad8099c55f49d1089616f2374c17ccd2
Nath <nathalycis20@gmail.com>
2026-05-11 02:52:29 -0500

    Añadiendo clase Persona

--- BackEndParcial2/src/main/java/edu/udistrital/BackEnd/Model/Atleta.java
+++ BackEndParcial2/src/main/java/edu/udistrital/BackEnd/Model/Atleta.java
@@ -4,10 +4,29 @@
  */
 package edu.udistrital.BackEnd.Model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Data;
+
 /**
  *
- * @author natha
+ * @author nath
  */
+@Entity
+@EqualsAndHashCode(callSuper = true)
+@Table(name = "atletas")
+@Data
 public class Atleta {
+    
+    
+    private Integer edad;
+
+    private String categoria;
+
+    private String especialidad;
+
+    private Boolean modalidadCross;
+    
+    private String foto;
     
 }

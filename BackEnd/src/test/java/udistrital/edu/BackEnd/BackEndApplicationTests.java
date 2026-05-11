e6f76df0e0cecd0c262b1d966c11228fc88c0e9c
Nath <nathalycis20@gmail.com>
2026-05-11 02:17:14 -0500

    Corrigiendo carpeta

---
+++ BackEnd/src/test/java/udistrital/edu/BackEnd/BackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package udistrital.edu.BackEnd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
